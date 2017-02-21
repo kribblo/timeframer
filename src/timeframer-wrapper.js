81f3b359c59c9f15585107bb605fefe695349b2a
Kristoffer Lundén <kristoffer.lunden@gmail.com>
2017-02-21 19:52:49 +0100

    separate into browserify module, build standalone for dist

---
+++ src/timeframer-wrapper.js
@@ -0,0 +1,11 @@
+var timeframer = require('./timeframer');
+
+var scripts = document.getElementsByTagName('script');
+var index = scripts.length - 1;
+var tag = scripts[index];
+var src = tag.src;
+var root = src.substring(0, src.lastIndexOf('/'));
+
+var frameSrc = root + '/timeframer.html';
+
+timeframer(frameSrc, window);
